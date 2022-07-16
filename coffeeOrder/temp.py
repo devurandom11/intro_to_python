d7e297ac09ab8d376fcc3044b88d86253144a90e
Mike Crawford <dev.random.87@gmail.com>
2022-07-16 01:25:32 -0700

    Modify functions

--- coffeeOrder/temp.py
+++ coffeeOrder/temp.py
@@ -20,7 +20,15 @@ def main():
         total = sum(drinks.values()) 
 #       print (total)
     print('We are closed...')
-
+    # print_dict(drinks)
+    # print_list(agents)
+    # print_pref(preference)
+    # print_agents(agents)
+    # print_drinks(drinks)
+    print (drinks)
+    print (agents)
+    print (preference)
+    
 # Customer order
 def order():
     customer_name = input('What is your name? ')
@@ -54,6 +62,38 @@ def order():
         drinks[preference[customer_name.capitalize()]] -= -1
         return drinks
 
-         
+# Remember the customer orders and preferences in a dictionary
+# Print the dictionary
+# Print the total number of drinks left
+# Print the list of agents
+# Print the list of drinks
+# Print the list of preferences
+
+def print_dict(d):
+    for k,v in d.items():
+        print(f'{k} : {v}')
+    print()
+    
+def print_list(l):
+    for i in l:
+        print(i)
+    print()
+
+def print_pref(d):
+    for k,v in d.items():
+        print(f'{k} : {v}')
+    print()
+
+def print_agents(l):
+    for i in l:
+        print(i)
+    print()
+
+def print_drinks(l):
+    for i in l:
+        print(i)
+    print()
+
+
 if __name__ == "__main__":
     main()
